e6b4ba77a57ab5470b2d2850df10f16370b3314a
liuyantao <liuyantao0206@163.com>
2017-11-23 20:47:48 +0800

    1111

---
+++ src/main/java/com/jk/controller/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.jk.controller.controller;
+
+
+import com.jk.controller.rabbia.hello.HelloSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Lenovo on 2017/11/17.
+ */
+@RestController
+public class TestController {
+
+    @Autowired
+    private HelloSender helloSender;
+
+    @RequestMapping("/sendMassger")
+    public void sendMassger(){
+        for (int i = 0 ; i < 100 ; i ++){
+            helloSender.send();
+        }
+    }
+
+}
